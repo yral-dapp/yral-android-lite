ea1e64adf7fc4cac854b9bcbd36353ef4515e588
saikat-das-yral <79742177+saikat-das-yral@users.noreply.github.com>
2024-09-25 09:25:01 +0000

    build: add android app project boilerplate

---
+++ app/src/main/java/com/yral/lite/DelegationService.java
@@ -0,0 +1,14 @@
+package com.yral.lite;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
